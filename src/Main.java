b4cefae4b964af15e2a2f439bb6b0a178cabc8e0
Alysson <alyssonmths5@gmail.com>
2025-11-06 11:21:01 -0300

    adicionando lógica de teste na classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import FactoryMethod.Item.Item;
+import FactoryMethod.Item.Tipo;
+import FactoryMethod.Pedido;
 import FactoryMethod.PedidoDigitalFactory;
 import FactoryMethod.PedidoFactory;
 import FactoryMethod.PedidoFisicoFactory;
 
 public class Main {
     public static void main(String[] args) {
-        PedidoFactory pedidoDigital = new PedidoDigitalFactory();
-        PedidoFactory pedidoFisico = new PedidoFisicoFactory();
+        PedidoFactory factoryDigital = new PedidoDigitalFactory();
+        PedidoFactory factoryFisico = new PedidoFisicoFactory();
+
+        Pedido pedidoDigital = factoryDigital.instanciarPedido();
+        pedidoDigital.adicionarItem(new Item(1, "notebook", 1, Tipo.FISICO));
+        pedidoDigital.adicionarItem(new Item(2, "monitor", 1, Tipo.FISICO));
+        System.out.println(pedidoDigital);
     }
 }
