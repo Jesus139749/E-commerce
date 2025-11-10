ccec6514b30d4fc275363dacc41f2bc615f559bb
Alysson <alyssonmths5@gmail.com>
2025-11-09 23:19:58 -0300

    ajustes no Carrinho

--- src/carrinho/Carrinho.java
+++ src/carrinho/Carrinho.java
@@ -1,7 +1,55 @@
 package carrinho;
 
-import pedido.Pedido;
+import item.Item;
+import item.Tipo;
+import pedido.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Carrinho {
-    public Pedido pedido;
+
+    private List<Item> itens = new ArrayList<>();
+    private double valor;
+
+    public void adicionarItem(Item item) {
+        this.itens.add(item);
+        this.atualizarValor();
+    }
+
+    public void removerItem(int id) {
+        this.itens.removeIf(item -> item.id == id);
+        this.atualizarValor();
+    }
+
+    public void atualizarValor() {
+        double valorAtualizado = 0;
+        for (Item item : itens) {
+            valorAtualizado += item.valor;
+        }
+        this.valor = valorAtualizado;
+    }
+
+    public List<Pedido> gerarPedido() {
+
+        List<Item> itensDigitais = itens.stream().filter(item -> item.tipo.equals(Tipo.DIGITAL)).toList();
+        List<Item> itensFisicos = itens.stream().filter(item -> item.tipo.equals(Tipo.FISICO)).toList();
+
+        List<Pedido> pedidos = new ArrayList<>();
+
+        if (!itensDigitais.isEmpty()) {
+            Pedido pedidoDigital = new PedidoDigitalFactory().instanciarPedido();
+            pedidoDigital.setarItens(itensDigitais);
+            pedidos.add(pedidoDigital);
+        }
+
+        if (!itensFisicos.isEmpty()) {
+            Pedido pedidoFisico = new PedidoFisicoFactory().instanciarPedido();
+            pedidoFisico.setarItens(itensFisicos);
+            pedidos.add(pedidoFisico);
+        }
+
+        return pedidos;
+
+    }
 }
