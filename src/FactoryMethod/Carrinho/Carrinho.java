aec462b7236f92b34abf175230bf2d0f5bf50671
Alysson <alyssonmths5@gmail.com>
2025-11-09 22:15:13 -0300

    criação do package carrinho

--- src/FactoryMethod/Carrinho/Carrinho.java
+++
@@ -1,7 +0,0 @@
-package FactoryMethod.Carrinho;
-
-import FactoryMethod.Pedido;
-
-public class Carrinho {
-    public Pedido pedido;
-}
---
+++ src/carrinho/Carrinho.java
@@ -0,0 +1,7 @@
+package carrinho;
+
+import pedido.Pedido;
+
+public class Carrinho {
+    public Pedido pedido;
+}
