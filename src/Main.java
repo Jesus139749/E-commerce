e72292a6d65548007229e17150f96e0fda541af6
Alysson <alyssonmths5@gmail.com>
2025-11-09 22:15:45 -0300

    atualização do main

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
-import FactoryMethod.Item.Item;
-import FactoryMethod.Item.Tipo;
-import FactoryMethod.Pedido;
-import FactoryMethod.PedidoDigitalFactory;
-import FactoryMethod.PedidoFactory;
-import FactoryMethod.PedidoFisicoFactory;
+import item.Item;
+import item.Tipo;
+import pedido.Pedido;
+import pedido.PedidoDigitalFactory;
+import pedido.PedidoFactory;
+import pedido.PedidoFisicoFactory;
 
 public class Main {
     public static void main(String[] args) {
+
         PedidoFactory factoryDigital = new PedidoDigitalFactory();
         PedidoFactory factoryFisico = new PedidoFisicoFactory();
 
         Pedido pedidoDigital = factoryDigital.instanciarPedido();
-        pedidoDigital.adicionarItem(new Item(1, "notebook", 1, Tipo.FISICO));
-        pedidoDigital.adicionarItem(new Item(2, "monitor", 1, Tipo.FISICO));
+        pedidoDigital.adicionarItem(new Item(1, "notebook", 1, 2000, Tipo.DIGITAL));
+        pedidoDigital.adicionarItem(new Item(2, "monitor", 1, 500, Tipo.DIGITAL));
+        pedidoDigital.removerItem(2);
+
         System.out.println(pedidoDigital);
     }
 }
