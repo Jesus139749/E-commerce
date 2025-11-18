4f038e9709942e05473520b13692e1485719301b
Alysson <alyssonmths5@gmail.com>
2025-11-18 13:51:56 -0300

    Implementação de pagamento por BoletoNacional

--- src/pagamento/BoletoNacional.java
+++ src/pagamento/BoletoNacional.java
@@ -3,6 +3,23 @@ package pagamento;
 public class BoletoNacional implements BoletoInterface {
     @Override
    public void pagar(double valor){
-        System.out.println("Pagando BoletoInterface Nacional...");
+        if (valor <= 0) {
+            System.out.println("Valor do boleto inválido!");
+            return;
+        }
+
+        System.out.println("Gerando boleto nacional");
+        System.out.printf("Valor: R$ %.2f %n", valor);
+
+        System.out.println("Processando pagamento...");
+
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e) {
+            System.out.println("Erro ao processar pagamento: " + e);
+        }
+
+        System.out.println("Pagamento do boleto realizado com sucesso!");
     }
 }
