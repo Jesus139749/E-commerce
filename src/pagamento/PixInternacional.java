e489352d9218d75466ba2a3f761ca9845d777067
Alysson <alyssonmths5@gmail.com>
2025-11-19 11:27:57 -0300

    Implementação de método para pagamento via PIX

--- src/pagamento/PixInternacional.java
+++ src/pagamento/PixInternacional.java
@@ -1,9 +1,30 @@
 package pagamento;
 
 public class PixInternacional implements PixInterface {
+
+    private final double TAXA_IOF = 0.035;
+
     @Override
     public void  pagar(double valor){
-        System.out.println("Pagando PixInterface Internacional....");
+
+        double iof = valor * TAXA_IOF;
+        double total = valor + iof;
+
+        System.out.println("=== Pagamento Internacional por PIX ===");
+        System.out.printf("Valor base: R$ %.2f%n", valor);
+        System.out.printf("IOF (3.5%%): R$ %.2f%n", iof);
+        System.out.printf("Valor final: R$ %.2f %n", total);
+
+        System.out.println("Processando pagamento...");
+
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e) {
+            System.out.println("Erro ao processar pagamento: " + e);
+        }
+
+        System.out.println("Pagamento via PIX realizado com sucesso!");
     }
 
 }
--- src/pagamento/PixNacional.java
+++ src/pagamento/PixNacional.java
@@ -3,6 +3,19 @@ package pagamento;
 public class PixNacional implements PixInterface {
     @Override
     public void pagar(double valor){
-        System.out.println("Pagando PixInterface Nacional...");
+
+        System.out.println("=== Pagamento Nacional por PIX ===");
+        System.out.printf("Valor total: R$ %.2f%n", valor);
+
+        System.out.println("Processando pagamento...");
+
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e) {
+            System.out.println("Erro ao processar pagamento: " + e);
+        }
+
+        System.out.println("Pagamento via PIX realizado com sucesso!");
     }
 }
